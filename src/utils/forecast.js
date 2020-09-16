0c8707bb031ef039876c1f7d11b1530af1a76f8e
Jishnu Kumar <apple@Apples-MacBook-Pro.local>
2020-09-16 17:35:59 +0530

    modified weather forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -21,7 +21,7 @@ const forecast = (latitude,longitude,callback)=>{
             callback('unable to find the location. Please enter the valid co-ordinates',undefined)
 
         }else{
-            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+ body.current.temperature + ' degrees Out.'+ ' it feels like '+ body.current.feelslike+ ' degrees out.'
+            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+ body.current.temperature + ' degrees Out.'+ ' it feels like '+ body.current.feelslike+ ' degrees out.' + ' The Humidity is '+body.current.humidity 
 
             )
         }
